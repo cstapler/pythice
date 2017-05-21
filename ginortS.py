e43198c4f976d610a8b1bb378238bdd8d6cbf959
Chris Stapler <cstapler@iastate.edu>
2017-05-21 18:30:27 -0500

    Change to user pep8/google python style guid conventions

--- ginortS.py
+++ ginortS.py
@@ -1,22 +1,33 @@
+"""
+ hackerrank python ginortS challenge solution.
+
+"""
 import string
 
 
-def wordFirstLittleFirstOddFirst(c):
-    if c.isalpha():
-        if c.islower():
-            cVal = string.ascii_lowercase.index(c)
-            return cVal
-        else:
-            cVal = string.ascii_uppercase.index(c)
-            return cVal + 1000
-    else:
-        cInt = int(c)
-        if cInt % 2 == 0:
-            return cInt + 100000
-        else:
-            return cInt + 10000
+def _word_first_lower_first_odd_first(current_char):
+    """Key function for sorting based on ginortS specified rules."""
+
+    if current_char.isalpha():
+        if current_char.islower():
+            c_val = string.ascii_lowercase.index(current_char)
+            return c_val
+        c_val = string.ascii_uppercase.index(current_char)
+        return c_val + 1000
+    c_int = int(current_char)
+    if c_int % 2 == 0:
+        return c_int + 100000
+    return c_int + 10000
 
 
-inputString = input()
-outputString = sorted(inputString, key=wordFirstLittleFirstOddFirst)
-print(*outputString, sep='')
+def main():
+    """Take inputted string and returns sorted string based on ginortS
+    challenge rules.
+    """
+    input_string = input()
+    output_string = sorted(input_string, key=_word_first_lower_first_odd_first)
+    print(*output_string, sep='')
+
+
+if __name__ == "__main__":
+    main()
